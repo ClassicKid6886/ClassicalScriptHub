0e522e6c529697bc0dcd8a1e0a1f45d746e71223
ClassicKid6886 <102368409+ClassicKid6886@users.noreply.github.com>
2025-01-11 15:14:43 +0200

    Update Bootstrapper.lua

--- Bootstrapper.lua
+++ Bootstrapper.lua
@@ -1,1 +1,26 @@
 print("Installing Classical Hub for Slap Battles")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+if game.PlaceId == 9431156611 then
+local Window = OrionLib:MakeWindow({Name = "Classical Hub - Slap Royal", HidePremium = false, SaveConfig = true, ConfigFolder = "ClassicHubSlapRoyalConfig"})
+
+local function SendError(Message)
+    OrionLib:MakeNotification({
+        Name = "Unexpected Error",
+        Content = Message,
+        Image = "rbxassetid://12472046162",
+        Time = 5
+    })
+end
+
+
+else
+    OrionLib:MakeNotification({
+        Name = "Unexpected Error",
+        Content = "Classic Hub is not in the right place...",
+        Image = "rbxassetid://12472046162",
+        Time = 5
+    })
+end
+
+OrionLib:Init()
