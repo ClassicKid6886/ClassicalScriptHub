6f6fb138b8c7efa79a9d047ddaab792d3d85cea8
ClassicKid6886 <102368409+ClassicKid6886@users.noreply.github.com>
2025-01-11 19:45:10 +0200

    Update Bootstrapper.lua

--- Bootstrapper.lua
+++ Bootstrapper.lua
@@ -1,18 +1,15 @@
 print("Installing Classical Hub for Slap Battles")
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
---if game.PlaceId == 9431156611 then
-
 Rayfield:Notify({
     Title = "Loading...",
     Content = "Classic Hub is loading. This will only take a moment...",
     Duration = 6.5,
     Image = "loader",
- })
+})
 
---loadstring(game:HttpGet(('')))()
-
-local GoToItemsFunction = loadstring(game:HttpGet(('https://github.com/ClassicKid6886/ClassicalScriptHub/raw/refs/heads/SlapRoyal/GoToItems.lua')))()
+local GoToItemsFunction = loadstring(game:HttpGet('https://github.com/ClassicKid6886/ClassicalScriptHub/raw/refs/heads/SlapRoyal/GoToItems.lua'))()
+local FlyFunction = loadstring(game:HttpGet('https://github.com/ClassicKid6886/ClassicalScriptHub/raw/refs/heads/Storage/UniversalFly_Velocity.lua'))()
 
 local function SendError(Message)
     Rayfield:Notify({
@@ -20,48 +17,76 @@ local function SendError(Message)
         Content = Message,
         Duration = 6.5,
         Image = "ban",
-     })
+    })
 end
 
 local Window = Rayfield:CreateWindow({
     Name = "Classical Hub - Slap Royal",
-    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+    Icon = 0,
     LoadingTitle = "Welcome to Classical Hub - Slap Royal",
     LoadingSubtitle = "Getting thing ready...",
-    Theme = "DarkBlue", -- Check https://docs.sirius.menu/rayfield/configuration/themes
- 
-    DisableRayfieldPrompts = false,
-    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
- 
+    Theme = "DarkBlue",
     ConfigurationSaving = {
-       Enabled = true,
-       FolderName = nil, -- Create a custom folder for your hub/game
-       FileName = "ClassicHubSlapRoyal"
+        Enabled = true,
+        FolderName = nil,
+        FileName = "ClassicHubSlapRoyal"
     },
- 
     Discord = {
-       Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+        Enabled = false,
+        Invite = "noinvitelink",
+        RememberJoins = true
     },
- 
-    KeySystem = false, -- Set this to true to use our key system
+    KeySystem = false,
     KeySettings = {
-       Title = "Untitled",
-       Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+        Title = "Untitled",
+        Subtitle = "Key System",
+        Note = "No method of obtaining the key is provided",
+        FileName = "Key",
+        SaveKey = true,
+        GrabKeyFromSite = false,
+        Key = {"Hello"}
     }
- })
+})
+
+local GeneralTab = Window:CreateTab("General", "command")
+
+local FlySection = GeneralTab:CreateSection("Fly")
+local SliderFlySpeed = 10
+local Slider = FlySection:CreateSlider({
+    Name = "Fly Speed",
+    Range = {10, 300},
+    Increment = 2,
+    Suffix = " Max. 300",
+    CurrentValue = 10,
+    Flag = "FlySpeedValue",
+    Callback = function(Value)
+        SliderFlySpeed = Value * 10
+    end,
+})
+
+local FlyToggle = FlySection:CreateToggle({
+    Name = "Toggle Fly",
+    CurrentValue = false,
+    Flag = "FlyToggleValue",
+    Callback = function(Value)
+        FlyFunction(Value, SliderFlySpeed)
+    end,
+})
 
 local ExtraTab = Window:CreateTab("Extra", "anvil")
 
 ExtraTab:CreateButton({
-	Name = "Go To Items",
-	Callback = function()
-      		GoToItemsFunction()
-  	end    
+    Name = "Go To Items",
+    Callback = function()
+        GoToItemsFunction()
+    end
+})
+
+local ScriptTab = Window:CreateTab("Script", "scroll")
+
+ScriptTab:CreateButton({
+    Name = "Kill Script",
+    Callback = function()
+        Rayfield:Destroy()
+    end
 })
