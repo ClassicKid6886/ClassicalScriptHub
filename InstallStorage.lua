2080f63f30fdf493459f8ba92efb2ba5190f2553
ClassicKid6886 <102368409+ClassicKid6886@users.noreply.github.com>
2025-01-11 15:00:09 +0200

    Update InstallStorage.lua

--- InstallStorage.lua
+++ InstallStorage.lua
@@ -1,4 +1,9 @@
-{
-		id = 12472046162,
-		link = "https://raw.githubusercontent.com/ClassicKid6886/ClassicalScriptHub/refs/heads/SlapRoyal/Bootstrapper.lua"
+local InstallStorage = {
+    {
+        id = 12472046162,
+        link = "https://raw.githubusercontent.com/ClassicKid6886/ClassicalScriptHub/refs/heads/SlapRoyal/Bootstrapper.lua"
+    },
+    -- You can add more entries here if needed
 }
+
+return InstallStorage
