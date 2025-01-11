b6512d4885f0aa306ac7134797f7a28d78fd5acc
ClassicKid6886 <102368409+ClassicKid6886@users.noreply.github.com>
2025-01-11 15:50:01 +0200

    Update Bootstrapper.lua

--- Bootstrapper.lua
+++ Bootstrapper.lua
@@ -2,7 +2,17 @@ print("Installing Classical Hub for Slap Battles")
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 if game.PlaceId == 9431156611 then
-local Window = OrionLib:MakeWindow({Name = "Classical Hub - Slap Royal", HidePremium = false, SaveConfig = true, ConfigFolder = "ClassicHubSlapRoyalConfig"})
+
+OrionLib:MakeNotification({
+    Name = "Loading...",
+    Content = "Classic Hub is loading. This will only take a moment...",
+    Image = "rbxassetid://12472046162",
+    Time = 5
+})
+
+--loadstring(game:HttpGet(('')))()
+
+local GoToItemsFunction = loadstring(game:HttpGet(('https://github.com/ClassicKid6886/ClassicalScriptHub/raw/refs/heads/SlapRoyal/GoToItems.lua')))()
 
 local function SendError(Message)
     OrionLib:MakeNotification({
@@ -12,6 +22,22 @@ local function SendError(Message)
         Time = 5
     })
 end
+
+local Window = OrionLib:MakeWindow({Name = "Classical Hub - Slap Royal", HidePremium = false, IntroText = "Welcome to Classical Hub - Slap Royal", SaveConfig = true, ConfigFolder = "ClassicHubSlapRoyalConfig"})
+
+local ExtraTab = Window:MakeTab({
+	Name = "Extra",
+	Icon = "rbxassetid://10696045527",
+	PremiumOnly = false
+})
+
+ExtraTab:AddButton({
+	Name = "Go To Items",
+	Callback = function()
+      		GoToItemsFunction()
+  	end    
+})
+
 
 
 else
