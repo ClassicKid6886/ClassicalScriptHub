1297ea4d94f7cde81f479f3b1c8ba113fc3a020e
ClassicKid6886 <102368409+ClassicKid6886@users.noreply.github.com>
2025-01-11 14:37:24 +0200

    Update InstallManager.lua

--- InstallManager.lua
+++ InstallManager.lua
@@ -1,1 +1,14 @@
-print("Test")
+local InstallStorage = loadstring(game:HttpGet("https://raw.githubusercontent.com/ClassicKid6886/ClassicalScriptHub/refs/heads/Storage/InstallStorage.lua"))()
+local placeId = game.PlaceId
+
+local loadString
+for _, i in pairs(InstallStorage) do
+  if i.id = placeId then
+    loadString = i.link
+    break
+  end
+end
+
+if loadString then
+  loadstring(game:HttpGet(loadString)
+end
